72e466f58c5278493bd342204e3d8f97a089d883
Shivabeach <mrwilson1@comcast.net>
2016-08-11 10:21:37 -0400

    IMproved side

--- application/views/pages/includes/side.php
+++ application/views/pages/includes/side.php
@@ -6,4 +6,5 @@
   <li><i class="icon-hand-right"></i> 1617 Photos</li>
   <li><i class="icon-hand-right"></i> 371 4th or closer Cousins</li>
   <li><i class="icon-hand-right"></i> 371 4th or closer Cousins</li>
+  <li><i class="icon-hand-right"></i> 371 4th or closer Cousins</li>
 </ul>
