dd3e6425b940fad368654adb997ff4e93ac950fd
Brad VanHorn <mrwilson1@comcast.net>
2016-08-17 21:43:14 -0400

    Login system

--- application/controllers/Checkin.php
+++ application/controllers/Checkin.php
@@ -17,5 +17,27 @@ class Checkin extends CI_Controller{
     $this->load->view('pages/login/log', $data);
     $this->load->view('pages/footer/footer');
   }
+  public function process()
+  {
+    $data = array(
+      'name'      => html_escape($this->input->post('name')),
+      'password'  => html_escape($this->input->post('password')),
+      'password2' => html_escape($this->input->post('password2')),
+      'email'     => html_escape($this->input->post('email')),
+      );
+      $this->form_validation->set_rules('name', 'name', 'required');
+      $this->form_validation->set_rules('password', 'password', 'required');
+      $this->form_validation->set_rules('password2', 'password2', 'required|matches[password2]');
+      $this->form_validation->set_rules('email', 'email', 'required|valid_email');
 
-}
+      if( $this->form_validation->run() == FALSE) {
+  			echo validation_errors();
+  		}else
+  		{
+  			$this->db->insert('check', $data);
+  			echo "grand shit";
+  		}
+
+  } //end of process
+
+} //end of controller
--- application/views/pages/login/log.php
+++ application/views/pages/login/log.php
@@ -7,10 +7,54 @@
     <h3>Register or log in</h3>
     <fieldset>
       <legend>Login</legend>
-      <?php
-        
-      ?>
 
+      <?php
+      $attr = [
+        'id'    => 'ajax',
+        "class" => "pure-form pure-form-stacked"
+      ];
+      echo form_open("/checkin/process", $attr)?>
+<p>
+
+<label for="name"> name </label> <br />
+<input type="text" name="name" id="name" />
+<p/>
+
+<p>
+  <?php
+  $attr1 = [
+    'style' => 'width: 50%',
+    'id' => 'password',
+    'name' => 'password'
+];
+   ?>
+<label for="password"> password </label> <br />
+<?php echo form_input($attr1);?>
+<p/>
+
+<p>
+  <?php
+  $attr2 = [
+    'style' => 'width: 50%',
+    'id' => 'password2',
+    'name' => 'password2'
+];
+   ?>
+<label for="password2"> password2 </label> <br />
+<?php echo form_input($attr2);?>
+<p/>
+
+<p>
+<label for="email"> email </label> <br />
+<input type="email" name="email" id="email" />
+<p/>
+
+<div class="pure-controls">
+  <?php
+  echo '<button type="submit" class="pure-button pure-button-active">Submit</button>';
+  echo '<button type="reset" class="pure-button pure-button-active">Reset</button>';
+  echo form_close();
+  ?>
 
     </fieldset>
   </div>
